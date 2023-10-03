eee28625197c711b22d30f5f8890bab5e1b9f3b8
Nadya <nam198374@gmail.com>
2023-10-03 11:48:00 +0200

    works with inline tokens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
+import {Subscription} from 'rxjs';
+import {CurrentWeather, CurrentWeatherService} from './services/current-weather.service';
+import {LocationService, UserLocation} from "./services/location.service";
+import {ForecastFiveDays, ForecastFiveDaysService} from "./services/forecast-five-days.service";
+
+export interface UserPlace{
+  userAddress: string,
+  userLatitude: string,
+  userLongitude: string,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-weather-forecast';
+
+  userPlace: UserPlace = {
+    userAddress: '',
+    userLongitude: '',
+    userLatitude: ''
+  }
+
+  inputModel: any;
+  constructor(public service: CurrentWeatherService,
+              private locationService: LocationService) {}
+
+  currentWeather: CurrentWeather | null = null;
+  forecast : ForecastFiveDays | null = null;
+
+  userLocation: UserLocation | null = null;
+
+  subscription: Subscription = undefined;
+  subscriptionForecast: Subscription = undefined;
+  subscriptionLocation: Subscription = undefined;
+
+  ngOnInit() {
+    this.subscription = this.service.currentWeather$
+      .subscribe(res => this.currentWeather = res);
+
+    this.subscriptionForecast = this.service.weatherForecast$
+      .subscribe(res =>this.forecast = res);
+
+    this.subscriptionLocation = this.locationService.getUserLocation()
+      .subscribe(res=> {
+        this.userPlace.userAddress = res.city;
+        this.userPlace.userLatitude = res.latitude;
+        this.userPlace.userLongitude = res.longitude;
+        console.log("UserLocation", this.userPlace)
+        this.service.setCurrentPlace(this.userPlace);
+      })
+
+  }
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+    this.subscriptionLocation?.unsubscribe();
+  }
+  handleAddressChange (address?:any) {
+    this.userPlace.userAddress = address.formatted_address;
+    this.userPlace.userLongitude = address.geometry.location.lng()
+    this.userPlace.userLatitude = address.geometry.location.lat()
+    console.log('Place:', this.userPlace);
+    this.service.setCurrentPlace(this.userPlace);
+    this.inputModel = '';
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete';
 
 import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { ForecastFiveDaysComponent } from './forecast-five-days/forecast-five-days.component';
+import { ForecastPreviewComponent } from './forecast-preview/forecast-preview.component';
+import {FormsModule} from "@angular/forms";
+import { DailyForecastComponent } from './daily-forecast/daily-forecast.component';
+import { FillerPipe } from './filler.pipe';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentWeatherComponent,
+    ForecastFiveDaysComponent,
+    ForecastPreviewComponent,
+    DailyForecastComponent,
+    FillerPipe,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        GooglePlaceModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
